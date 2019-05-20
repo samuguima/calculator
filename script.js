b18ef638bb56aca61f74fb67af72186d53321110
samuguima <samuelguima2@gmail.com>
2019-05-20 16:21:18 -0300

    função soma, função subtração

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function soma(a, b){
+    valor= parseInt(a) + parseInt(b);
+    document.getElementById("resultSoma").innerHTML=valor;
+}
+
+function sub(a, b){
+    valor= parseInt(a) - parseInt(b);
+    document.getElementById("resultSub").innerHTML=valor;
+}
+
+texto = "Bem vindo ao Calculator";
+alert(texto);
