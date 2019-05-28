2dfc772f832f81151ea4c6d6551c9ea03660f13c
samuguima <samuelguima2@gmail.com>
2019-05-28 17:04:56 -0300

    caixa seletora

--- script.js
+++ script.js
@@ -1,21 +1,113 @@
+function trocaMoeda(){
+
+    if ( $("#selecionarMoeda").val() == "$" ){
+
+        $('#vlrNumero1').maskMoney({
+            thousands: ',',
+            decimal: '.',
+            allowZero:true,
+            prefix: '$'
+        });
+        
+        $('#vlrNumero2').maskMoney({
+            thousands: ',',
+            decimal: '.',
+            allowZero:true,
+            prefix: '$'
+        });
+
+    }
+
+    
+}
+
+function trocaMoeda(){
+
+    if ( $("#selecionarMoeda").val() == "€" ){
+
+        $('#vlrNumero1').maskMoney({
+            thousands: '.',
+            decimal: ',',
+            allowZero:true,
+            prefix: '€'
+        });
+        
+        $('#vlrNumero2').maskMoney({
+            thousands: '.',
+            decimal: ',',
+            allowZero:true,
+            prefix: '€'
+        });
+
+    }
+
+    
+}
+
+function trocaMoeda(){
+
+    if ( $("#selecionarMoeda").val() == "R$" ){
+
+        $('#vlrNumero1').maskMoney({
+            thousands: '.',
+            decimal: ',',
+            allowZero:true,
+            prefix: 'R$'
+        });
+        
+        $('#vlrNumero2').maskMoney({
+            thousands: '.',
+            decimal: ',',
+            allowZero:true,
+            prefix: 'R$'
+        });
+
+    }
+
+    
+}
+
+function trocaMoeda(){
+
+    if ( $("#selecionarMoeda").val() == "¥" ){
+
+        $('#vlrNumero1').maskMoney({
+            allowZero:true,
+            prefix: '¥'
+        });
+        
+        $('#vlrNumero2').maskMoney({
+            allowZero:true,
+            prefix: '¥'
+        });
+
+    }
+
+
+}
+
 
 function soma(a, b){
-    valor= parseInt(a) + parseInt(b);
+    valor= parseFloat(a) + parseFloat(b);
+    valor = valor.toFixed(2);
     document.getElementById("resultNumero").innerHTML=valor;
 }
 
 function sub(a, b){
-    valor= parseInt(a) - parseInt(b);
+    valor= parseFloat(a) - parseFloat(b);
+    valor = valor.toFixed(2);
     document.getElementById("resultNumero").innerHTML=valor;
 }
 
 function mult(a, b){
-    valor= parseInt(a) * parseInt(b);
+    valor= parseFloat(a) * parseFloat(b);
+    valor = valor.toFixed(2);
     document.getElementById("resultNumero").innerHTML=valor;
 }
 
 function div(a, b){
-    valor= parseInt(a) / parseInt(b);
+    valor= parseFloat(a) / parseFloat(b);
+    valor = valor.toFixed(2);
     document.getElementById("resultNumero").innerHTML=valor;
 }
 
